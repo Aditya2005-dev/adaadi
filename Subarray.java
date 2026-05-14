8ebf45de3ab3ee4887d903c66831ebf21bec6e29
Aditya kulkarni <kulkarniadi09@gmail.com>
2026-05-14 19:54:54 +0530

    Updated Subarray

--- Subarray.java
+++ Subarray.java
@@ -4,16 +4,40 @@ public class Subarray {
 
     void sss() {
 
+        int max = 0;
+
+        int start = 0;
+        int end = 0;
+
         for (int i = 0; i < arr.length; i++) {
 
             for (int j = i; j < arr.length; j++) {
 
+                int product = 1;
+
                 for (int k = i; k <= j; k++) {
+
                     System.out.print(arr[k] + " ");
+                    product = product * arr[k];
                 }
 
+                if (product > max) {
+                    max = product;
+                    start = i;
+                    end = j;
+                }
+
+                System.out.print(" -> Product = " + product);
                 System.out.println();
             }
+        }
+
+        System.out.println("\nMaximum Product = " + max);
+
+        System.out.print("Subarray = ");
+
+        for (int i = start; i <= end; i++) {
+            System.out.print(arr[i] + " ");
         }
     }
 
@@ -21,6 +45,5 @@ public class Subarray {
 
         Subarray a = new Subarray();
         a.sss();
-
     }
 }
